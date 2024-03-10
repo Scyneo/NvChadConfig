ec15d16c3c5ebe28b721f0cb702e3a82d8f51ea4
Mateusz Babicz <mateuszbabiczlol@gmail.com>
2024-03-10 22:13:46 +0100

    initial commit

---
+++ lua/plugins.lua
@@ -0,0 +1,35 @@
+local plugins = {
+  {
+    "williamboman/mason.nvim",
+    opts = {
+      ensure_installed = {
+        "gopls",
+      },
+    },
+  },
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      require ('nvchad.configs.lspconfig').defaults()
+      require "configs.lspconfig"
+    end,
+  },
+  {
+    "jose-elias-alvarez/null-ls.nvim",
+    ft = "go",
+    opt = function()
+      return require "configs.null-ls"
+    end,
+  },
+  {
+    "olexsmir/gopher.nvim",
+    ft = "go",
+    config = function(_, opts)
+      require("gopher").setup(opts)
+    end,
+    build = function()
+      vim.cmd [[silent! GoInstallDeps]]
+    end,
+  },
+}
+return plugins
